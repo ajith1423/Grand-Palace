532f73e06ee077f107b6ccf883d2b63c5f8a0ff3
ajith1423 <ajithmano14@gmail.com>
2026-02-25 15:07:00 +0530

    server

--- backend/server.py
+++ backend/server.py
@@ -3775,8 +3775,10 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=[
+        "https://grand-palace.onrender.com"
+    ],
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
     allow_methods=["*"],
     allow_headers=["*"],
 )
